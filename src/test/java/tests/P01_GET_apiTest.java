0d17b410dc1b31affd156dc10028e690e1318904
Zeynep Hasret Kırbaş <email@example.com>
2025-02-24 14:34:20 +0300

    first commit

---
+++ src/test/java/tests/P01_GET_apiTest.java
@@ -0,0 +1,53 @@
+package tests;
+
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class P01_GET_apiTest {
+
+    /*
+        https://restful-booker.herokuapp.com/booking/10 url’ine
+        bir GET request gonderdigimizde donen Response’un,
+        status code’unun 200,
+        ve content type’inin application/json; charset=utf-8,
+        ve Server isimli Header’in degerinin Cowboy,
+        ve status Line’in HTTP/1.1 200 OK
+        ve response suresinin 5 sn’den kisa oldugunu manuel olarak test ediniz.
+     */
+
+    /*
+        Tum API sorgulari 4 asamada yapilir:
+
+            1- EndPoint hazirlanir (url ve VARSA (PUT-POST-PATCH) request body hazirlanir.
+            2- Soruda verilmisse expected data hazirlanir. Verilmemisse hazirlanmaz
+            3- Donen cevap kaydedilir
+            4- Assertion islemleri yapilir
+     */
+
+    @Test
+    public void test01(){
+
+        // 1- EndPoint hazirlanir
+        String url = "https://restful-booker.herokuapp.com/booking/10";
+
+        // 2- Soruda verilmisse expected data hazirlanir. Verilmemisse hazirlanmaz
+        // Soruda verilmemis
+
+        // 3- Donen cevap kaydedilir
+        Response response = given().when().get(url);
+
+        // response.prettyPrint();
+        // response.prettyPeek();
+
+        // 4- Assertion islemleri yapilir
+        System.out.println("Status Degeri: " + response.getStatusCode()); // 200
+        System.out.println("Content Type Degeri: " + response.getContentType()); // application/json; charset=utf-8
+        System.out.println("Server Isimli Header Degeri: " + response.getHeader("Server")); // Cowboy
+        System.out.println("Status Line Degeri: " + response.getStatusLine()); // HTTP/1.1 200 OK
+        System.out.println("Response Suresi Degeri: " + response.getTime()); // 5 saniyeden kisa
+
+    }
+
+}
---
+++ src/test/java/tests/P03_JSONDataOlusturma.java
@@ -0,0 +1,50 @@
+package tests;
+
+public class P03_JSONDataOlusturma {
+
+    /*
+    Asagidaki JSON Objesini olusturup konsolda yazdirin.
+
+        {
+        "title":"Ahmet",
+        "body":"Merhaba",
+        "userId":1
+        }
+    */
+
+
+
+
+
+
+
+
+
+
+
+
+
+    /*
+        Asagidaki JSON Objesini olusturup konsolda yazdirin.
+
+        {
+            "firstname":"Jim",
+                "additionalneeds":"Breakfast",
+                "bookingdates": {
+            "checkin":"2018-01-01",
+                    "checkout":"2019-01-01"
+        },
+            "totalprice":111,
+                "depositpaid":true,
+                "lastname":"Brown"
+        }
+    */
+
+    /*
+        Bu gibi ic ice veri bulunduran datalarla calisirken
+        once en iceridekinden baslanir.
+     */
+
+
+
+}
